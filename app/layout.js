27cd66f0a048f1ca14da113c307f0ca7084be959
aliwarisnaqvi125-spec <aliwarisnaqvi125@gmail.com>
2026-03-14 08:45:23 -0700

    head tag add

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="monetag" content="7091fd85dda71e1175a3595237936c71" />
+      </head>
       <body className={inter.variable}>
         <Navbar />
         <main>{children}</main>
